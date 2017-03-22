8bdb4f1b3861ecdedb364ff442ebbf9fbaca5cfb
vagrant <vagrant@vagrant.vm>
2017-03-22 07:42:12 +0000

    Updated controller with new and create methods. Also created a private method for job parameters.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,21 @@
 class JobsController < ApplicationController
     def index
     end
+
+    def new
+        @job = Job.new
+    end
+
+    def create
+        p "In the create method!!"
+        job = Job.create(job_params)
+        p job
+        redirect_to jobs_path
+    end
+
+    private
+    def job_params
+        params.require(:job).permit(:title, :description)
+    end
+
 end
