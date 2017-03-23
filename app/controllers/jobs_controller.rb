f0c7917ae083e318856f25e4f58f5e6801b125ed
Stefanie Moy <ssmores@hotmail.com>
2017-03-23 07:27:01 +0000

    Created delete (destroy) method.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -24,6 +24,13 @@ class JobsController < ApplicationController
         redirect_to jobs_path
     end
 
+    def destroy
+        @job = Job.find(params[:id])
+        @job.destroy
+        redirect_to jobs_path
+    end
+
+
     private
 
     def job_params
